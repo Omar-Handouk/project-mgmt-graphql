94de259181759d098dfd9782d9fb6e07b6dcc309
Omar-Handouk <omar.handouk@ibm.com>
2022-06-17 23:52:04 +0200

    Add a simple logger

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-import dotenv   from 'dotenv';
-import morgan   from 'morgan';
-import helmet   from 'helmet';
-import express  from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import express from 'express';
 import mongoose from 'mongoose';
 
-import { graphqlHTTP }  from 'express-graphql';
+import { graphqlHTTP } from 'express-graphql';
 
-import config               from './config/default.js';
-import schema               from './schema/schema.js';
-import applicationRoutes    from './routes/index.js';
-import simpleLogger         from './utils/simplelogger.js';
+import config from './config/default.js';
+import schema from './schema/schema.js';
+import applicationRoutes from './routes/index.js';
+import simpleLogger from './utils/simplelogger.js';
 
 dotenv.config();
 global.logger = simpleLogger;
 
-const applicationHostAddress    = process.env.HOST;
-const applicationPort           = parseInt(process.env.PORT);
-const applicationEnv            = process.env.NODE_ENV;
-const databaseURI               = process.env.MONGO_URI;
-const databaseUser              = process.env.MONGO_USER;
-const databasePass              = process.env.MONGO_PASS;
-const morganOutputConfig        = applicationEnv === 'dev' ? 'dev' : 'common';
-const app                       = express();
-const apiBasePath               = config.apiBasePath;
+const applicationHostAddress = process.env.HOST;
+const applicationPort = parseInt(process.env.PORT);
+const applicationEnv = process.env.NODE_ENV;
+const databaseURI = process.env.MONGO_URI;
+const databaseUser = process.env.MONGO_USER;
+const databasePass = process.env.MONGO_PASS;
+const morganOutputConfig = applicationEnv === 'dev' ? 'dev' : 'common';
+const app = express();
+const apiBasePath = config.apiBasePath;
 
-app.use(helmet({ contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false }));
+app.use(helmet({
+    contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false
+}));
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://0.0.0.0:3000'
+    ]
+}));
 app.use(morgan(morganOutputConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,19 +45,27 @@ app.use('/graphql', graphqlHTTP({
     graphiql: applicationEnv === 'dev'
 }));
 
-app.listen(applicationPort, applicationHostAddress, async () => {
-    global.logger(`Attempting to connect to database cluster ${databaseURI}`);
-    try {
-        await mongoose.connect(databaseURI, {
-            dbName: 'dev',
-            user: databaseUser,
-            pass: databasePass,
-        });
-    } catch (e) {
-        global.logger(e.stack, 'error');
-        process.exit(1);
-    }
-    global.logger(`Connection to database cluster ${databaseURI} is successful`);
+(async () => {
+    await new Promise((resolve, reject) => {
+        global.logger(`Attempting to connect to database cluster ${databaseURI}`);
 
-    global.logger(`Server is running @ http://${applicationHostAddress}:${applicationPort}`);
-});
+        try {
+            mongoose.connect(databaseURI, {
+                dbName: 'dev',
+                user: databaseUser,
+                pass: databasePass,
+            });
+
+            global.logger(`Connection to database cluster ${databaseURI} is successful`);
+            resolve();
+        } catch (e) {
+            global.logger(e.stack, 'error');
+            reject(e);
+            process.exit(1);
+        }
+    });
+
+    app.listen(applicationPort, applicationHostAddress, () => {
+        global.logger(`Server is running @ http://${applicationHostAddress}:${applicationPort}`);
+    });
+})();
--- utils/simplelogger.js
+++ utils/simplelogger.js
@@ -1,10 +1,34 @@
+import chalk from 'chalk';
+
 /**
  * 
  * @param {string} level Log Level
  * @param {string} msg Log Message
  */
 const simpleLogger = (msg, level = 'info') => {
-    console.log(`[${level.toLocaleUpperCase()}][${new Date().toISOString()}] - ${msg}`);
+    const normalizedLevel = level.toLocaleLowerCase();
+    let style = undefined;
+
+    switch (normalizedLevel) {
+        case 'debug':
+            style = chalk.white.bgGreen;
+            break;
+        case 'info':
+            style = chalk.white.bgCyan;
+            break;
+        case 'warn':
+            style = chalk.white.bgYellow;
+            break;
+        case 'error':
+            style = chalk.white.bgHex('#FFA500')
+            break;
+        case 'fatal':
+            style = chalk.white.bgRed;
+            break;
+        default:
+    }
+    
+    console.log(`${style(`[${normalizedLevel}]`)}[${new Date().toISOString()}] - ${msg}`);
 }
 
 export default simpleLogger;
