2b1089d5b8f5463c289ef53e23b4996664f4f58d
Omar-Handouk <omar.handouk@ibm.com>
2022-07-02 17:12:42 +0200

    Added support to production build of react

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import cors from 'cors';
+import path from 'path';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import helmet from 'helmet';
@@ -6,6 +8,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 import { graphqlHTTP } from 'express-graphql';
+import { RESET_CONTENT, StatusCodes } from 'http-status-codes';
 
 import config from './config/default.js';
 import schema from './schema/schema.js';
@@ -21,12 +24,16 @@ const applicationEnv = process.env.NODE_ENV;
 const databaseURI = process.env.MONGO_URI;
 const databaseUser = process.env.MONGO_USER;
 const databasePass = process.env.MONGO_PASS;
+const databaseName = process.env.MONGO_DB_NAME;
+const reactAppURL = process.env.REACT_APP_URL ?? 'http://127.0.0.1:3000/';
+const buildPath = path.join(path.resolve(), 'build');
 const morganOutputConfig = applicationEnv === 'dev' ? 'dev' : 'common';
+
 const app = express();
 const apiBasePath = config.apiBasePath;
 
 app.use(helmet({
-    contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false
+    contentSecurityPolicy: process.env.NODE_ENV === 'production'
 }));
 app.use(cors({
     origin: [
@@ -39,11 +46,38 @@ app.use(morgan(morganOutputConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (applicationEnv === 'production') {
+    app.use(express.static(buildPath));
+}
+
 app.use(apiBasePath, applicationRoutes);
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: applicationEnv === 'dev'
 }));
+
+app.use('*', (_req, res) => {
+    if (applicationEnv === 'dev') {
+        return res.status(StatusCodes.MOVED_PERMANENTLY).redirect(reactAppURL);
+    }
+
+    if (fs.existsSync(buildPath)) {
+        try {
+            const indexPath = path.join(buildPath, 'index.html');
+            if (!fs.existsSync(indexPath)) {
+                throw new Error(`${indexPath} does not exist!`);
+            }
+
+            return res.status(StatusCodes.OK).sendFile(indexPath);
+        } catch (e) {
+            console.error(e);
+            process.exit(1);
+        }
+
+    } else {
+        throw new Error(`${buildPath} does not exist!`);
+    }
+});
 
 (async () => {
     await new Promise((resolve, reject) => {
@@ -51,7 +85,7 @@ app.use('/graphql', graphqlHTTP({
 
         try {
             mongoose.connect(databaseURI, {
-                dbName: 'dev',
+                dbName: databaseName,
                 user: databaseUser,
                 pass: databasePass,
             });
